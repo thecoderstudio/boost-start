08a8b934299e0248bb7a654eb6ae0606eb491b36
Robin Siep <RobinSiep1@gmail.com>
2017-12-28 16:37:39 +0100

    more prompting and writing

--- react/index.js
+++ react/index.js
@@ -1,17 +1,34 @@
 import {Generator} from 'yeoman-generator';
 
 class ReactGenerator extends Generator {
+  val userInput;
+
+  constructor(args, opts) {
+    super(args, opts);
+    this.sourceRoot('./templates/base');
+  }
+
   prompting() {
     return this.prompt([
       {
         type: "input",
-        name: "project_name",
+        name: "projectName",
         message: "Your project name"
       },
       {
         type: "input",
-        name: "project_description",
+        name: "projectDescription",
         message: "Your project description"
+      },
+      {
+        type: "input",
+        name: "title",
+        message: "Your project title"
+      },
+      {
+        type: "input",
+        name: "author",
+        message: "Author"
       },
       {
         type: "list",
@@ -21,8 +38,31 @@ class ReactGenerator extends Generator {
         default: 0,
       }
     ]).then((answers) => {
-
+      userInput = answers
     });
+  }
+
+  writing() {
+    this.fs.copyTpl(
+      this.templatePath('package.json'),
+      this.destinationPath('package.json'),
+      {
+        name: userInput.projectName,
+        description: userInput.projectDescription,
+        author: userInput.author
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('src/index.html'),
+      this.destinationPath('src/index.html'),
+      {
+        title: userInput.title
+      }
+    );
+    this.fs.copyTpl(
+      this.templatePath('./**'),
+      this.destionationPath('.')
+    );
   }
 }
 
