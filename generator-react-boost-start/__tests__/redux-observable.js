a96898f4b847a243b5a4a94c8e143f97476e04f6
Robin Siep <RobinSiep1@gmail.com>
2018-01-12 12:02:56 +0100

    redux-observable test

--- generator-react-boost-start/__tests__/redux-observable.js
+++ generator-react-boost-start/__tests__/redux-observable.js
@@ -3,28 +3,53 @@ const path = require('path');
 const assert = require('yeoman-assert');
 const helpers = require('yeoman-test');
 
+const storeCode = `\
+import { createStore } from 'redux';
 
-describe('generator:app', () => {
+import reducer from '../reducers';
+
+function configureStore() {
+  const store = createStore(reducer);
+
+  return { store };
+}
+
+export default configureStore;
+`;
+
+
+describe('generator:redux-observable', () => {
   describe('defaults', () => {
     beforeEach(() => {
-      return helpers.run(path.join(__dirname, '../generators/docker'))
+      return helpers.run(path.join(__dirname, '../generators/redux-observable'))
+        .withPrompts({
+          'redux-observable': true
+        })
         .withOptions({
           projectName: "test-react-app",
           destinationRoot: "test-react-app/"
+        })
+        .on('ready', (generator) => {
+          generator.fs.write('src/store/index.jsx', storeCode);
         });
     });
 
     it('creates files', () => {
       const expected = [
-        'Dockerfile'
+        'src/epics/index.jsx'
       ];
 
       assert.file(expected);
     });
 
-    it('fills Dockerfile with correct information', () => {
-      assert.fileContent('Dockerfile', 'RUN mkdir -p test-react-app');
-      assert.fileContent('Dockerfile', 'WORKDIR /test-react-app');
+    it('fills store/index.jsx with correct information', () => {
+      assert.fileContent('src/store/index.jsx', "import { createStore, applyMiddleware } from 'redux';");
+      assert.fileContent('src/store/index.jsx', "import { createEpicMiddleware } from 'redux-observable';");
+      assert.fileContent('src/store/index.jsx', "import epic from '../epics';");
+      assert.fileContent('src/store/index.jsx', 'const epicMiddleware = createEpicMiddleware(epic);');
+      assert.fileContent('src/store/index.jsx', "const store = createStore(");
+      assert.fileContent('src/store/index.jsx', "reducer,");
+      assert.fileContent('src/store/index.jsx', "applyMiddleware(epicMiddleware)");
     });
   });
 });
